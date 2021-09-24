f5afe7e77b367befaa0ed98af114422bd95ad59f
dinaabuzeena <dinamohammad735@gmail.com>
2021-09-24 15:56:25 +0300

    test

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
   }
 
   handleLocation = (e) => {
+    // console.log(e)
     let display_name = e.target.value;
     this.setState({
       display_name: display_name
